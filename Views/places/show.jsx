4453d1ed9103f388d5463d47eb1c4a585a817507
Luis Donis <luis@Luiss-MBP-2.lan>
2022-10-08 18:38:38 -0700

    Creating Helper Method

---
+++ Views/places/show.jsx
@@ -0,0 +1,54 @@
+const React = require('react')
+const Def = require('../default')
+
+function show (data) {
+    return (
+        <Def>
+          <main>
+            <div className="row">
+              <div className="col-sm-6">
+                <img src={data.place.pic} alt={data.place.name} />
+                <h3>
+                  Located in {data.place.city}, {data.place.state}
+                </h3>
+              </div>
+              <div className="col-sm-6">
+                <h1>{ data.place.name }</h1>
+                <h2>
+                  Rating
+                </h2>
+                <h3 className="inactive">
+                  Not Rated
+                </h3>
+                <br />
+                <h2>
+                  Description
+                </h2>
+                <h3>
+                  {data.place.showEstablished()}
+                </h3>
+                <h4>
+                  Serving {data.place.cuisines}
+                </h4>
+                <br />
+                <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
+                  Edit
+                </a>{` `}
+                <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
+                  <button type="submit" className="btn btn-danger">
+                    Delete
+                  </button>
+                </form>
+              </div>
+            </div>
+            <hr />
+            <h2>Comments</h2>
+            <h3 className="inactive">
+              No comments yet!
+            </h3>
+          </main>
+        </Def>
+    )
+}
+
+module.exports = show
--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-//Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
-//Express Settings 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
-//Controllers & routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
